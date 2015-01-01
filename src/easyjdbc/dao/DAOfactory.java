9817981c9c68224d2b7e6ae0af18d856a6bbfeaa
parksungho <parksungho86@gmail.com>
2015-01-01 19:59:50 +0900

    testclass remove

--- src/easyjdbc/dao/DAOfactory.java
+++ src/easyjdbc/dao/DAOfactory.java
@@ -2,15 +2,11 @@ package easyjdbc.dao;
 
 public class DAOfactory {
 	
-	public static boolean TEST = false;
-	
 	private DAOfactory(){
 	}
 	
 	
 	public static DAO getDAO() {
-		if(TEST)
-			return new TestDAO();
 		return new MySql();
 	}
 }
