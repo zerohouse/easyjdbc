3cb31df0a5f6efb65e0ae0dadfdfa87f3c0b0c2d
parksungho <parksungho86@gmail.com>
2015-01-22 18:28:11 +0900

    g

--- src/easyjdbc/query/raw/GetRecordQuery.java
+++ src/easyjdbc/query/raw/GetRecordQuery.java
@@ -3,14 +3,17 @@ package easyjdbc.query.raw;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import easyjdbc.query.Query;
 
 public class GetRecordQuery extends Query {
-
+	
 	public List<Object> execute(Connection conn) {
 		List<Object> record = new ArrayList<Object>();
 		PreparedStatement pstmt;
@@ -25,6 +28,42 @@ public class GetRecordQuery extends Query {
 				for (int i = 0; i < resultSize; i++) {
 					record.add(rs.getObject(i + 1));
 				}
+			}
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException sqle) {
+				}
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException sqle) {
+				}
+		} catch (SQLException e) {
+			System.out.println(sql);
+			System.out.println(parameters);
+			e.printStackTrace();
+		}
+		return record;
+	}
+
+	public Map<String, Object> getMap(Connection conn) {
+		Map<String, Object> record = new LinkedHashMap<String, Object>();
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			if (parameters != null)
+				for (int j = 0; j < parameters.size(); j++) {
+					pstmt.setObject(j + 1, parameters.get(j));
+				}
+			ResultSet rs = pstmt.executeQuery();
+			ResultSetMetaData metaData = rs.getMetaData();
+			int columnCount = metaData.getColumnCount();
+
+			while (rs.next()) {
+			    for (int i = 1; i <= columnCount; i++) {
+			    	record.put(metaData.getColumnLabel(i), rs.getObject(i));
+			    }
 			}
 			if (pstmt != null)
 				try {
--- src/easyjdbc/query/raw/GetRecordsQuery.java
+++ src/easyjdbc/query/raw/GetRecordsQuery.java
@@ -3,14 +3,17 @@ package easyjdbc.query.raw;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import easyjdbc.query.Query;
 
 public class GetRecordsQuery extends Query {
-
+	
 	public List<List<Object>> execute(Connection conn) {
 		List<Object> record;
 		List<List<Object>> result = new ArrayList<List<Object>>();
@@ -29,6 +32,46 @@ public class GetRecordsQuery extends Query {
 				}
 				result.add(record);
 			}
+			if (pstmt != null)
+				try {
+					pstmt.close();
+				} catch (SQLException sqle) {
+				}
+			if (rs != null)
+				try {
+					rs.close();
+				} catch (SQLException sqle) {
+				}
+		} catch (SQLException e) {
+			System.out.println(sql);
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	public List<Map<String, Object>> getMapList(Connection conn) {
+		List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+		PreparedStatement pstmt;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			if (parameters != null)
+				for (int j = 0; j < parameters.size(); j++) {
+					pstmt.setObject(j + 1, parameters.get(j));
+				}
+			ResultSet rs = pstmt.executeQuery();
+			
+			ResultSetMetaData metaData = rs.getMetaData();
+			int columnCount = metaData.getColumnCount();
+
+			while (rs.next()) {
+			    Map<String, Object> columns = new LinkedHashMap<String, Object>();
+			    for (int i = 1; i <= columnCount; i++) {
+			        columns.put(metaData.getColumnLabel(i), rs.getObject(i));
+			    }
+			    result.add(columns);
+			}
+			
 			if (pstmt != null)
 				try {
 					pstmt.close();
